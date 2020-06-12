98819c16ca8d2a52f527a13a4aefd5ea3f4602fb
Home <kiporukmaksim@gmail.com>
2020-06-11 22:11:59 -0700

    les

---
+++ les_1/variable.py
@@ -0,0 +1,7 @@
+name_student = "Maksym"
+age = 21
+print("name of student {0}. {1}".format(name_student, age))
+second_student = input("Pleas, inpunt your first name")
+second_student_age = int(input("input youe are(only nameric)"))
+print("name of second student {0}. {1} ".format(second_student, second_student_age))
+
